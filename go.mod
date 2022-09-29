3fbe0126d35bc247bdd553bb7ec057e6911e64b8
Talor Itzhak <titzhak@redhat.com>
2022-09-29 16:39:24 +0300

    init repo
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

---
+++ go.mod
@@ -0,0 +1,45 @@
+module github.com/Tal-or/rps-debug
+
+go 1.18
+
+require (
+	github.com/openshift-kni/cnf-features-deploy v0.0.0-20220928145739-5314ce12b1c7
+	k8s.io/klog v1.0.0
+	k8s.io/kubernetes v1.25.2
+)
+
+require (
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/golang/glog v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	k8s.io/klog/v2 v2.80.1 // indirect
+)
+
+replace (
+	k8s.io/api => k8s.io/api v0.24.1
+	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.24.1
+	k8s.io/apimachinery => k8s.io/apimachinery v0.24.1
+	k8s.io/apiserver => k8s.io/apiserver v0.24.1
+	k8s.io/cli-runtime => k8s.io/cli-runtime v0.24.1
+	k8s.io/client-go => k8s.io/client-go v0.24.1
+	k8s.io/cloud-provider => k8s.io/cloud-provider v0.24.1
+	k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.24.1
+	k8s.io/code-generator => k8s.io/code-generator v0.24.1
+	k8s.io/component-base => k8s.io/component-base v0.24.1
+	k8s.io/component-helpers => k8s.io/component-helpers v0.24.1
+	k8s.io/controller-manager => k8s.io/controller-manager v0.24.1
+	k8s.io/cri-api => k8s.io/cri-api v0.24.1
+	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.24.1
+	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.24.1
+	k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.24.1
+	k8s.io/kube-proxy => k8s.io/kube-proxy v0.24.1
+	k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.24.1
+	k8s.io/kubectl => k8s.io/kubectl v0.24.1
+	k8s.io/kubelet => k8s.io/kubelet v0.24.1
+	k8s.io/kubernetes => k8s.io/kubernetes v1.24.1
+	k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.24.1
+	k8s.io/metrics => k8s.io/metrics v0.24.1
+	k8s.io/mount-utils => k8s.io/mount-utils v0.24.1
+	k8s.io/pod-security-admission => k8s.io/pod-security-admission v0.24.1
+	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.24.1
+)
---
+++ oslat-runner/main.go
@@ -0,0 +1,66 @@
+package oslat_runner
+
+import (
+	"flag"
+	"os/exec"
+	"time"
+
+	"k8s.io/klog"
+	"k8s.io/kubernetes/pkg/kubelet/cm/cpuset"
+
+	"github.com/openshift-kni/cnf-features-deploy/cnf-tests/pod-utils/pkg/node"
+)
+
+const oslatBinary = "/usr/bin/oslat"
+
+func main() {
+	klog.InitFlags(nil)
+
+	var oslatStartDelay = flag.Int("oslat-start-delay", 0, "Delay in second before running the oslat binary, can be useful to be sure that the CPU manager excluded the pinned CPUs from the default CPU pool")
+	var rtPriority = flag.String("rt-priority", "1", "Specify the SCHED_FIFO priority (1-99)")
+	var runtime = flag.String("runtime", "10m", "Specify test duration, e.g., 60, 20m, 2H")
+
+	flag.Parse()
+
+	selfCPUs, err := node.GetSelfCPUs()
+	if err != nil {
+		klog.Fatalf("failed to get self allowed CPUs: %v", err)
+	}
+
+	if selfCPUs.Size() < 2 {
+		klog.Fatalf("the amount of requested CPUs less than 2, the oslat requires at least 2 CPUs to run")
+	}
+
+	mainThreadCPUs := selfCPUs.ToSlice()[0]
+	siblings, err := node.GetCPUSiblings(mainThreadCPUs)
+	if err != nil {
+		klog.Fatalf("failed to get main thread CPU siblings: %v", err)
+	}
+	cpusForLatencyTest := selfCPUs.Difference(cpuset.NewCPUSet(siblings...))
+	mainThreadCPUSet := cpuset.NewCPUSet(mainThreadCPUs)
+
+	err = node.PrintInformation()
+	if err != nil {
+		klog.Fatalf("failed to print node information: %v", err)
+	}
+
+	if *oslatStartDelay > 0 {
+		time.Sleep(time.Duration(*oslatStartDelay) * time.Second)
+	}
+
+	oslatArgs := []string{
+		"--duration", *runtime,
+		"--rtprio", *rtPriority,
+		"--cpu-list", cpusForLatencyTest.String(),
+		"--cpu-main-thread", mainThreadCPUSet.String(),
+	}
+
+	klog.Infof("Running the oslat command with arguments %v", oslatArgs)
+	out, err := exec.Command(oslatBinary, oslatArgs...).CombinedOutput()
+	if err != nil {
+		klog.Fatalf("failed to run oslat command; out: %s; err: %v", out, err)
+	}
+
+	klog.Infof("Succeeded to run the oslat command: %s", out)
+	klog.Flush()
+}
